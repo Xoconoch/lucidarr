e307acdfeb578905e3891a5c347ff012aa8ac6a9
cool.gitter.choco <gitter@cordovault.com>
2025-01-13 15:42:43 -0600

    Started frontend

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,37 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import checkModules from './routes/modules/checkModules.js';
 import addModule from './routes/modules/addModule.js';
-import removeModuleRoute from './routes/modules/removeModule.js';
-import searchRoute from './routes/search/searchRoute.js';
-import downloadArtistRoute from './routes/download/downloadArtist.js'; 
-import downloadAlbumRoute from './routes/download/downloadAlbum.js';
-import downloadTrackRoute from './routes/download/downloadTrack.js';
+import removeModule from './routes/modules/removeModule.js';
+import search from './routes/search/search.js';
+import downloadArtist from './routes/download/downloadArtist.js'; 
+import downloadAlbum from './routes/download/downloadAlbum.js';
+import downloadTrack from './routes/download/downloadTrack.js';
 
 const app = express();
-const port = 3000;
+const port = 3030;
 
 const router = express.Router();
 
 router.get('/modules/check', checkModules);
 router.post('/modules/add', addModule);
-router.delete('/modules/remove', removeModuleRoute);
-router.get('/search', searchRoute);
-router.get('/download/artist', downloadArtistRoute);  
-router.get('/download/album', downloadAlbumRoute);
-router.get('/download/track', downloadTrackRoute);
+router.delete('/modules/remove', removeModule);
+router.get('/search', search);
+router.get('/download/artist', downloadArtist);  
+router.get('/download/album', downloadAlbum);
+router.get('/download/track', downloadTrack);
 
 app.use(express.json());
 app.use('/api', router);
+
+const __filename = fileURLToPath(import.meta.url);  
+const __dirname = path.dirname(__filename);        
+
+const publicDirectory = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectory));
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
